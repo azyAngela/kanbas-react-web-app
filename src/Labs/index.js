927d8aa8f2f5143d33e372f0404ab63fc0882552
Angela An <angelaan@angelas-air.mynetworksettings.com>
2023-10-27 14:10:34 -0400

    lab

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,14 +1,43 @@
-import { Link } from "react-router-dom";
-import Assignment3 from "./a3";
+// import { Link } from "react-router-dom";
+// import Assignment3 from "./a3";
+// import Nav from "../Nav";
+// import PathParameters from "./a3/PathParameters";
+// function Labs() {
+//    return (
+//       <div className="container">
+//          <Nav />
+//          <Assignment3 />
+//          <PathParameters />
+//       </div>
+//    );
+// }
+// export default Labs;
+
 import Nav from "../Nav";
-import PathParameters from "./a3/PathParameters";
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import {Routes, Route, Navigate} from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
+
+
 function Labs() {
-   return (
-      <div className="container">
-         <Nav />
-         <Assignment3 />
-         <PathParameters />
-      </div>
-   );
+ return (
+  <Provider store={store}>
+   <div>
+     <Nav/>
+     <Routes>
+       <Route path="/"
+        element={<Navigate
+                  to="a3"/>}/>
+       <Route path="a3"
+        element={<Assignment3/>}/>
+       <Route path="a4"
+        element={<Assignment4/>}/>
+     </Routes>
+   </div>
+  </Provider>
+ );
 }
+
 export default Labs;
