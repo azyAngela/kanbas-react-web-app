625fdfc7ee23303883c71a010ecb9f47b1afbdc7
Ziyi An <ziyia2@uw.edu>
2023-11-20 18:50:29 -0500

    link

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+// const MODULES_URL = "http://localhost:4000/api/modules";
+// const MODULES_URL = "https://kanbas-node-server-app-amul.onrender.com/api/modules";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const MODULES_URL = `${API_BASE}/modules`;
 export const deletesModule = async (moduleId) => {
   const response = await axios
     .delete(`${MODULES_URL}/${moduleId}`);
