9a77dee1568fb2c424e5a64337b6dd22d9fdfcd6
Ziyi An <ziyia2@uw.edu>
2023-12-01 23:51:57 -0500

    update link

--- src/project/users/client.js
+++ src/project/users/client.js
@@ -7,46 +7,46 @@ const request = axios.create({
   
 export const signin = async (credentials) => {
     //   const response = await axios.post( `${USERS_API}/signin`, credentials );
-    const response = await request.post(`http://localhost:4000/api/users/signin`, credentials);
+    const response = await request.post(`https://kanbas-node-server-app2-dolc.onrender.com/api/users/signin`, credentials);
     return response.data;
 };
 export const accountUser = async () => {
-    const response = await request.post(`http://localhost:4000/api/users/accountUser`);
+    const response = await request.post(`https://kanbas-node-server-app2-dolc.onrender.com/api/users/accountUser`);
     return response.data;
 };
 export const updateUser = async (user) => {
-    const response = await request.put(`http://localhost:4000/api/users/${user._id}`, user);
+    const response = await request.put(`https://kanbas-node-server-app2-dolc.onrender.com/api/users/${user._id}`, user);
     return response.data;
 };
 
 export const findAllUsers = async () => {
-    const response = await request.get(`http://localhost:4000/api/users`);
+    const response = await request.get(`https://kanbas-node-server-app2-dolc.onrender.com/api/users`);
     return response.data;
   };
   
   export const createUser = async (user) => {
-    const response = await request.post(`http://localhost:4000/api/users`, user);
+    const response = await request.post(`https://kanbas-node-server-app2-dolc.onrender.com/api/users`, user);
     return response.data;
   };
 
   export const findUserById = async (id) => {
-    const response = await request.get(`http://localhost:4000/api/users/${id}`);
+    const response = await request.get(`https://kanbas-node-server-app2-dolc.onrender.com/api/users/${id}`);
     return response.data;
   };
   
   export const deleteUser = async (user) => {
     const response = await request.delete(
-      `http://localhost:4000/api/users/${user._id}`);
+      `https://kanbas-node-server-app2-dolc.onrender.com/api/users/${user._id}`);
     return response.data;
   };
   export const signup = async (credentials) => {
     const response = await request.post(
-      `http://localhost:4000/api/users/signup`, credentials);
+      `https://kanbas-node-server-app2-dolc.onrender.com/api/users/signup`, credentials);
     return response.data;
   };
   
   export const signout = async () => {
-    const response = await request.post(`http://localhost:4000/api/users/signout`);
+    const response = await request.post(`https://kanbas-node-server-app2-dolc.onrender.com/api/users/signout`);
     return response.data;
   };
   
